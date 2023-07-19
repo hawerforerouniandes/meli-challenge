0ac432dccd63f951ce8b4e8c75864e3bc6c874c0
hawerforerouniandes <hawerforerouniandes@gmail.com>
2023-07-19 16:23:46 -0500

    health status

--- src/main/java/com/challenge/meli/MeliChallengeApplication.java
+++ src/main/java/com/challenge/meli/MeliChallengeApplication.java
@@ -3,6 +3,8 @@ package com.challenge.meli;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableAsync
@@ -11,4 +13,11 @@ public class MeliChallengeApplication {
 		SpringApplication.run(MeliChallengeApplication.class, args);
 	}
 
+	@RestController
+	class HomeController {
+		@GetMapping("/")
+		public String health() {
+			return "Meli chanllege API!";
+		}
+	}
 }
